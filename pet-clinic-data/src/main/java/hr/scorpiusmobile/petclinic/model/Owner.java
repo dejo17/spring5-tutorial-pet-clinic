e0ee48f1a4931986d4e952dba029c763ee2cb716
Dean <dean.juricic@gmail.com>
2019-03-12 11:06:58 +0100

    Added Visit Entity, updated Owner Entity. Closes #26.

--- pet-clinic-data/src/main/java/hr/scorpiusmobile/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/hr/scorpiusmobile/petclinic/model/Owner.java
@@ -5,10 +5,21 @@
  */
 package hr.scorpiusmobile.petclinic.model;
 
+import java.util.Set;
+
 /**
  *
  * 
  */
 public class Owner extends Person{
-    
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
